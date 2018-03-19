9889ccdb25715fa6da3e0948b33a9b7dd6515e5b
Abel McElroy <abel.h.mcelroy@gmail.com>
2018-03-19 17:34:32 -0400

    deployment

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500).send(err.message || 'Internal server error');
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 db.sync()
     .then(() => {
